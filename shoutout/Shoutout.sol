f7390a3288d55c6ba19876fc0cdef727ac60b7b8
coinop-logan <coinop.logan@gmail.com>
2022-10-08 01:55:05 -0500

    Add simple contract

---
+++ shoutout/Shoutout.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Shoutout {
+    
+    event Shout(address author, string message);
+
+    function shoutout(string memory _message)
+        external
+    {
+        emit Shout(msg.sender, _message);
+    }
+}
